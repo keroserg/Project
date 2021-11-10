e1160c53269325b524ad0f38f81dfae19f40392b
Sergio Scaramuzzi <sergioscaramuzzi@gmail.com>
2021-11-10 10:06:09 -0600

    Documentation and comments added
    Added documentation and comments to the class. Should probably still be threaded and the actual value grab could stand to be re-thought.

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,10 +1,15 @@
+"""This Module scrapes weather data for max, min and mean."""
+
 from html.parser import HTMLParser
 import urllib.request
 from datetime import datetime
 
 class WeatherScraper(HTMLParser):
+    """Weather data HTML scraper."""
 
     def __init__(self):
+        """Intializes an instance of the WeatherScraper class."""
+
         super().__init__()
         self.tbody = False
         self.tr = False
@@ -16,6 +21,8 @@ class WeatherScraper(HTMLParser):
         self.lastPage = False
         
     def handle_starttag(self, tag, attrs):
+        """Handles the starttag event."""
+
         if tag == "tbody":
             self.tbody = True
         
@@ -25,15 +32,18 @@ class WeatherScraper(HTMLParser):
         if tag == "td" and self.tr == True:
             self.counter += 1
             self.td = True
-
+        
+        #Translates abbr tag into the desired date format.
         if tag == "abbr" and self.tr == True:
            self.rowDate = str(datetime.strptime(attrs[0][1], "%B %d, %Y").date())
 
+        #Detects last page.
         if len(attrs) == 2:
             if attrs[1][1] == "previous disabled":
                 self.lastPage = True
 
     def handle_endtag(self, tag):
+        """Handles the endtag event."""
         
         if tag == "td":
             self.td = False
@@ -43,14 +53,17 @@ class WeatherScraper(HTMLParser):
             self.tr = False
         
     def handle_data(self, data):
+        """Handles the data event."""
 
         if data == "Sum":
             self.tbody = False
-    
+        
+        #Populates daily_temps dict.
         if self.td == True and self.counter <= 3 and self.tbody == True:
             keys = ['Max', 'Min', 'Mean']
             self.daily_temps[keys[self.counter - 1]] = data
 
+        #Populates weather dict.
         if self.counter == 3:
             self.weather[self.rowDate] = self.daily_temps
             self.daily_temps = self.daily_temps.copy()
@@ -61,6 +74,7 @@ if __name__ == "__main__":
 
     today  = datetime.now()
 
+    #Figures out the starting URL and then updates the URL back as far as possible.
     while (weatherScrape.lastPage == False):
 
         Url = (f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day={today.day}&Year={today.year}&Month={today.month}#')
